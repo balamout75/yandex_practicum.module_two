efe73dcff676c9beb559d02441659fcff76a473a
Иван Васильев <balamout75@gmail.com>
2025-12-29 17:57:19 +0800

    Все получилось! не прошло и 16 часов

--- SprintEightMultiModuleGradle/ShopCatalogue/src/main/java/ru/yandex/practicum/security/CurrentUserIdArgumentResolver.java
+++ SprintEightMultiModuleGradle/ShopCatalogue/src/main/java/ru/yandex/practicum/security/CurrentUserIdArgumentResolver.java
@@ -1,19 +1,11 @@
 package ru.yandex.practicum.security;
 
 import org.springframework.core.MethodParameter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.ReactiveSecurityContextHolder;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.BindingContext;
 import org.springframework.web.reactive.result.method.HandlerMethodArgumentResolver;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
-import ru.yandex.practicum.dto.shoping.UserDto;
-import ru.yandex.practicum.service.shoping.UserService;
-
-import static reactor.netty.http.HttpConnectionLiveness.log;
 
 @Component
 public class CurrentUserIdArgumentResolver
